7f1233687d47ee86cc9e8973c4a73c59aa5bf848
Tentacle OS <>
2026-06-21 17:05:46 +0200

    feat: update session entry

---
+++ src/session/index.ts
@@ -0,0 +1,8 @@
+export { SessionManager } from './SessionManager.js';
+export type { ContextPressure } from './SessionManager.js';
+export { MemoryStore, memoryStore } from './MemoryStore.js';
+export type { MemoryEntry, RecentSession } from './MemoryStore.js';
+export { GoalManager, goalManager } from './GoalManager.js';
+export type { Goal } from './GoalManager.js';
+export { ContextStore, contextStore } from './ContextStore.js';
+export type { TaskContext } from './ContextStore.js';
